170e84b7b269b6e82141f4b2f5e06844558f11a0
mvila <mvila@voxelgroup.net>
2021-11-22 08:41:56 +0100

    Refactorized with the generalized solution. Kata solved

--- FiboTDD/Fibo.cs
+++ FiboTDD/Fibo.cs
@@ -16,30 +16,7 @@ namespace FiboTDD
                 return 1;
             }
 
-            if (n == 5)
-            {
-                return 5;
-            }
-
-            if (n == 6)
-            {
-                return 8;
-            }
-
-            if (n == 7)
-            {
-                return Calculate(5) + Calculate(6);
-            }
-
-            if (n == 8)
-            {
-                return Calculate(6) + Calculate(7);
-            }
-
-            if (n == 9)
-            {
-                return Calculate(7) + Calculate(8);
-            }
+            return Calculate(n - 1) + Calculate(n - 2);
 
         }
     }
